d0c99137e178d4f048959754be0ff3671c9a04da
Shivashish <dasshivashish11@gmail.com>
2021-12-22 01:04:34 +0530

    Added spaces

--- com/stack/Parser.java
+++ com/stack/Parser.java
@@ -5,39 +5,52 @@ import com.stack.exceptions.InvalidValueException;
 import java.io.IOException;
 
 public class Parser {
+
 	private final InputStream input;
 	private final Exec ex;
+
 	public Parser(InputStream is,Exec ex){
 		input=is;
 		this.ex=ex;
 	}
+
 	private enum Instructions {
 		INS_LOAD,
 	}
+
 	public boolean parseAndExec() throws IOException {
 		String read=input.getWord();
 		if (read==null) return false;
+
 		else if (read.length()==0) return true;
+
 		else {
 			int numArgs=0;
 			Instructions ins=null;
+
 			switch(read){
 				case "load":
 					ins=Instructions.INS_LOAD;
 					numArgs=1;
 					break;
+
 				case "add" :
 					ex.add();
 					return true;
+
 				case "print":
 					ex.print();
 					return true;
+
 				default:
 					throw new NoSuchInstructionException(read);
 			}
+
 			Number[] args=new Number[numArgs];
+
 			for (int i=0;i<numArgs;i++){
 				String tmp=input.getWord();
+
 				try
 				{
 					int num=Integer.parseInt(tmp);
@@ -54,12 +67,16 @@ public class Parser {
 					}
 				}
 			}
+
 			switch (ins){
 				case INS_LOAD :
 					ex.load(args);
 					break;
 			}
+
 		}
+
 		return true;
+
 	}
 }
